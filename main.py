757ad39c5b2687eba43ae7c1fcc2aa9dac235c4a
Luca Faggianelli <luca.faggianelli@gmail.com>
2016-04-27 00:05:34 +0200

    Add Category and Tag resources, enable debug

--- main.py
+++ main.py
@@ -47,10 +47,31 @@ class DocumentView(ResourceView):
     methods = [methods.Create, methods.Update,
             methods.Fetch, methods.List, methods.Delete]
 
+class Category(db.Document):
+    name = db.StringField(max_length=255, required=True)
+
+class CategoryResource(Resource):
+    document = Category
+
+@api.register(name='categories', url='/categories/')
+class CategoryView(ResourceView):
+    resource = CategoryResource
+    methods = [methods.List, methods.Create, methods.Delete]
+
+class Tag(db.Document):
+    name = db.StringField(max_length=255, required=True)
+
+class TagResource(Resource):
+    document = Tag
+
+@api.register(name='tags', url='/tags/')
+class TagView(ResourceView):
+    resource = TagResource
+    methods = [methods.List, methods.Create, methods.Delete]
 
 if __name__ == "__main__":
     port = 5000
     if len(sys.argv) > 2:
         port = int(sys.argv[1])
 
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', debug=True, port=port)
